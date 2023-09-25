b7536434682cbfa1ca133b6054407182745d48b7
Miguel <miguel@terranigma-solutions.com>
2023-09-25 12:57:44 +0200

    [ENH] Adapting tests a bit

--- tests/test_read_OMF.py
+++ tests/test_read_OMF.py
@@ -11,11 +11,14 @@ from subsurface import TriSurf
 from subsurface.visualization import to_pyvista_mesh, pv_plot
 from subsurface.writer import base_structs_to_binary_file
 
+"""
+Example how to read COLLINSTOWN OMF project
 
+"""
 @pytest.fixture(scope="module")
 def load_omf():
     config = dotenv_values()
-    path = config.get('PATH_TO_OMF2')
+    path = config.get('PATH_TO_COLLINSTOWN')
     omf = omfvista.load_project(path)
     return omf
 
@@ -27,7 +30,8 @@ def test_read_omf_with_pyvista(load_omf):
 
 def test_omf_to_unstruct_single_block(load_omf):
     omf = load_omf
-    polydata_obj: pyvista.PolyData = omf["GSB - BIF contacts"]
+    block_name = omf.get_block_name(4)
+    polydata_obj: pyvista.PolyData = omf[block_name]
     unstruct_pyvista: pyvista.UnstructuredGrid = polydata_obj.cast_to_unstructured_grid()
     cells_pyvista = unstruct_pyvista.cells.reshape(-1, 4)[:, 1:]
 
@@ -40,4 +44,4 @@ def test_omf_to_unstruct_single_block(load_omf):
 
     ts = TriSurf(mesh=unstruct)
     s = to_pyvista_mesh(ts)
-    pv_plot([s], image_2d=True)
+    pv_plot([s], image_2d=False)
