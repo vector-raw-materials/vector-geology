c01f1a844a4e8591a3a0e5a53117d7cd2aab7f93
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-26 10:18:53 +0200

    [DOC] endian

--- utils_scripts/deploy2.sh
+++ utils_scripts/deploy2.sh
@@ -5,7 +5,11 @@
 set -e
 
 # Load personal access token
-source .env
+# Load personal access token and remove any carriage return characters
+GIT_PERSONAL_ACCESS_TOKEN=$(grep -o 'GIT_PERSONAL_ACCESS_TOKEN=.*' .env | cut -f 2- -d '=' | tr -d '\r')
+
+# Stash the .env file to ignore it while switching branches
+git stash push .env
 
 # Switch to gh-pages branch
 git checkout gh-pages
@@ -22,5 +26,8 @@ git push https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/vector-raw-materials/ve
 
 # Switch back to the original branch
 git checkout -
+
+# Pop the stash to get back the .env file
+git stash pop
 
 echo "Documentation updated and pushed to gh-pages."
