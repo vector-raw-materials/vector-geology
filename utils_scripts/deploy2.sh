7ae586fc224c4aa68baf2c0f7c64582c70d1d417
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-26 10:05:39 +0200

    [DOC] Trying a new simple script to push the docs

---
+++ utils_scripts/deploy2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This script builds Sphinx documentation and pushes it to the gh-pages branch
+
+# Exit on error
+set -e
+
+# Load personal access token
+source .env
+
+# Switch to gh-pages branch
+git checkout gh-pages
+
+# Copy the generated HTML files to the root directory
+cp -r docs/build/html/* .
+
+# Add and commit the changes
+git add .
+git commit -m "Update documentation"
+
+# Use the personal access token to push changes
+git push https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/vector-raw-materials/vector-geology.git gh-pages
+
+# Switch back to the original branch
+git checkout -
+
+echo "Documentation updated and pushed to gh-pages."
