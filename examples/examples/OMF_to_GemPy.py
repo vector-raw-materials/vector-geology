264a92b203df9c74ca4d239fc78c8cb88db21d4b
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-11-20 09:17:13 +0100

    [DOC] Reading Collinstown to subsurface

--- examples/examples/OMF_to_GemPy.py
+++ examples/examples/OMF_to_GemPy.py
@@ -13,10 +13,11 @@ This tutorial demonstrates how to read an OMF project file in COLLINSTOWN.
 # Import the required libraries. 
 
 import omfvista
+import pandas as pd
 import pyvista
 import subsurface
-from subsurface import TriSurf
-from subsurface.visualization import to_pyvista_mesh, pv_plot
+from subsurface import TriSurf, LineSet
+from subsurface.visualization import to_pyvista_mesh, pv_plot, to_pyvista_line, init_plotter
 from subsurface.writer import base_structs_to_binary_file
 from dotenv import dotenv_values
 
@@ -50,28 +51,52 @@ if False:
 # Convert the loaded OMF project into an unstructured single block for further analysis.
 
 meshes = []
+lines = []
 for e in range(omf.n_blocks):
 
     block_name = omf.get_block_name(e)
     polydata_obj: pyvista.PolyData = omf[block_name]
     # Check if the polydata is a mesh and if is not continue
     print(polydata_obj.cell_type(0))
-    if polydata_obj.cell_type(0) != pyvista.CellType.TRIANGLE:
-        continue
-        
     unstruct_pyvista: pyvista.UnstructuredGrid = polydata_obj.cast_to_unstructured_grid()
-    cells_pyvista = unstruct_pyvista.cells.reshape(-1, 4)[:, 1:]
 
-    unstruct: subsurface.UnstructuredData = subsurface.UnstructuredData.from_array(
-        vertex=unstruct_pyvista.points,
-        cells=cells_pyvista,
-    )
+    grid = unstruct_pyvista 
+    cell_data = {name: grid.cell_data[name] for name in grid.cell_data}
+    match polydata_obj.cell_type(0):
+        case pyvista.CellType.TRIANGLE:
+            if False: continue # TODO: Remove this
+            
+            cells_pyvista = unstruct_pyvista.cells.reshape(-1, 4)[:, 1:]
+            new_cell_data = {
+                **{
+                    "Formation_Major_": e,
+                },
+                **cell_data
+            }
+            unstruct: subsurface.UnstructuredData = subsurface.UnstructuredData.from_array(
+                vertex=unstruct_pyvista.points,
+                cells=cells_pyvista,
+                cells_attr=pd.DataFrame(new_cell_data)
+            )
+            
+            ts = TriSurf(mesh=unstruct)
+            s = to_pyvista_mesh(ts)
+            meshes.append(s)
 
-    ts = TriSurf(mesh=unstruct)
-    s = to_pyvista_mesh(ts)
-    
-    meshes.append(s)
-    
+           
+        case pyvista.CellType.LINE:
+            if "Formation_Major" not in cell_data.keys(): continue
+            cells_pyvista = unstruct_pyvista.cells.reshape(-1, 3)[:, 1:]
+            unstruct: subsurface.UnstructuredData = subsurface.UnstructuredData.from_array(
+                vertex=unstruct_pyvista.points,
+                cells=cells_pyvista,
+                cells_attr=pd.DataFrame(cell_data)
+            )
+            line = LineSet(data=unstruct)
+            s = to_pyvista_line(line, radius=100, as_tube=True, spline=False)
+            
+            lines.append(s)
+
 if False:  # Replace with condition for exporting to Liquid Earth
     base_structs_to_binary_file("leapfrog1", unstruct)
 
@@ -80,19 +105,14 @@ if False:  # Replace with condition for exporting to Liquid Earth
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Use Subsurface and PyVista to visualize the unstructured data.
 
-import random
-def random_color_generator():
-    while True:
-        yield (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+plotter = init_plotter()
+for mesh in meshes[3:]:
+    plotter.add_mesh(mesh, cmap="magma", opacity=0.7)
 
-# Usage Example:
+for line in lines:
+    plotter.add_mesh(line, cmap="viridis", opacity=1)
 
-color_gen = random_color_generator()
+plotter.show()
 
-pv_plot(
-    meshes, 
-    image_2d=False, 
-    add_mesh_kwargs={ }
-)
 
 
