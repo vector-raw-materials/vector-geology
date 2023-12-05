9ba89073678e26b237038994105a0358ac8e5062
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-12-05 14:33:14 +0100

    [CLN] Extract functionality to functions

--- examples/examples/stonepark_gempy.py
+++ examples/examples/stonepark_gempy.py
@@ -10,7 +10,7 @@ Construct a 3D geological model of the Stonepark deposit using GemPy.
 
 import time
 
-import subsurface
+from vector_geology.stonepark_builder import initialize_geo_model, optimize_nuggets_for_whole_project, apply_optimized_nuggets
 
 start_time = time.time()  # start timer
 
@@ -42,163 +42,29 @@ for e, filename in enumerate(os.listdir(path)):
 # %%
 # Element 1 is an intrusion
 
-
 #  %%
 # Setup gempy object
-# structural_elements.pop(1)
-
-
-# Red range 0.7 cov 4
-structural_group_red = gp.data.StructuralGroup(
-    name="Red",
-    # elements=[structural_elements[i] for i in [0, 4, 6, 8]],
-    elements=[structural_elements[i] for i in [0, 4, 8]],
-    structural_relation=gp.data.StackRelationType.ERODE
-)
-
-# Any, Probably we can decimize this an extra notch
-structural_group_green = gp.data.StructuralGroup(
-    name="Green",
-    elements=[structural_elements[i] for i in [5]],
-    structural_relation=gp.data.StackRelationType.ERODE
-)
-
-# Blue range 2 cov 4
-structural_group_blue = gp.data.StructuralGroup(
-    name="Blue",
-    elements=[structural_elements[i] for i in [2, 3]],
-    structural_relation=gp.data.StackRelationType.ERODE
-)
-
-structural_group_intrusion = gp.data.StructuralGroup(
-    name="Intrusion",
-    elements=[structural_elements[i] for i in [1]],
-    structural_relation=gp.data.StackRelationType.ERODE
-)
-
-structural_groups = [structural_group_intrusion, structural_group_green, structural_group_blue, structural_group_red]
-structural_frame = gp.data.StructuralFrame(
-    structural_groups=structural_groups[2:],
-    color_gen=color_gen
-)
-# TODO: If elements do not have color maybe loop them on structural frame constructor?
-
-geo_model: gp.data.GeoModel = gp.create_geomodel(
-    project_name='Tutorial_ch1_1_Basics',
-    extent=global_extent,
-    resolution=[20, 10, 20],
-    refinement=6,  # * Here we define the number of octree levels. If octree levels are defined, the resolution is ignored.
-    structural_frame=structural_frame
-)
 
 # %% 
 # Add topography
 import xarray as xr
 dataset: xr.Dataset = xr.open_dataset(os.path.join(path, "Topography.nc"))
 
-gp.set_topography_from_arrays(
-    grid=geo_model.grid,
-    xyz_vertices= dataset.vertex.values
-)
-
-# gpv.plot_2d(geo_model, show_data=True)
-# gempy_vista = gpv.plot_3d(geo_model, show_data=True, show=False)
-
-
-# %% 
-# ## Optimize nuggets
-# In such a complex geometries often data does not fit perfectly. In order to account for this, GemPy allows to add a
-# small random noise to the data. This is done by adding a small random value to the diagonal of the covariance matrix.
-# We can optimize this value with respect to the condition number of the matrix. The condition number is a measure of
-# how well the matrix can be inverted. The higher the condition number, the worse the matrix can be inverted. This
-# means that the data is not well conditioned and the nugget should be increased. On the other hand, if the condition
-# number is too low, the data is overfitted and the nugget should be decreased. The optimal value is the one that
-# minimizes the condition number. This can be done with the following function:
+geo_model = initialize_geo_model(structural_elements, global_extent)
 
 # %%
-from vector_geology.model_building_functions import optimize_nuggets_for_group
-
-TRIGGER_OPTIMIZE_NUGGETS = False
-APPLY_OPTIMIZED_NUGGETS = True
-if TRIGGER_OPTIMIZE_NUGGETS:
-    
-    geo_model.interpolation_options.kernel_options.range = 0.7
-    geo_model.interpolation_options.kernel_options.c_o = 4
-    optimize_nuggets_for_group(
-        geo_model=geo_model,
-        structural_group=structural_group_red,
-        plot_evaluation=False,
-        plot_result=True
-    )
-    
-    geo_model.interpolation_options.kernel_options.range = 2
-    geo_model.interpolation_options.kernel_options.c_o = 4
-    
-    optimize_nuggets_for_group(
-        geo_model=geo_model,
-        structural_group=structural_group_blue,
-        plot_evaluation=False,
-        plot_result=False
-    )
-
-    if False:
-        optimize_nuggets_for_group(
-            geo_model=geo_model,
-            structural_group=structural_group_green,
-            plot_evaluation=False,
-            plot_result=True
-        )
-
-# %%
-if APPLY_OPTIMIZED_NUGGETS:
-    loaded_nuggets_red = np.load("nuggets_Red.npy")
-    loaded_nuggets_green = np.load("nuggets_Green.npy")
-    loaded_nuggets_blue = np.load("nuggets_Blue.npy")
-
-    gp.modify_surface_points(
-        geo_model,
-        slice=None,
-        elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Red').elements],
-        nugget=loaded_nuggets_red
-    )
-    
-    if True: # Ignore OB
-        gp.modify_surface_points(
-                geo_model,
-                slice=None,
-                elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Blue').elements],
-                nugget=loaded_nuggets_blue
-            )
-    if False:
-        gp.modify_surface_points(
-            geo_model,
-            slice=None,
-            elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Green').elements],
-            nugget=loaded_nuggets_green
-        )
 
 geo_model
 
 # %% 
-geo_model.interpolation_options.mesh_extraction = True
-geo_model.interpolation_options.kernel_options.range = .7
-geo_model.interpolation_options.kernel_options.c_o = 3
-geo_model.interpolation_options.kernel_options.compute_condition_number = True
+interpolation_options = geo_model.interpolation_options
 
-from gempy_engine.core.data.kernel_classes.kernel_functions import AvailableKernelFunctions
-geo_model.interpolation_options.kernel_options.kernel_function = AvailableKernelFunctions.cubic
+interpolation_options.mesh_extraction = True
+interpolation_options.kernel_options.range = .7
+interpolation_options.kernel_options.c_o = 3
+interpolation_options.kernel_options.compute_condition_number = True
 
 # %% 
-
-geo_model.structural_frame.get_element_by_name("Stonepark_KKR").color = "#A46283"
-geo_model.structural_frame.get_element_by_name("Stonepark_LGR").color = "#6394A4"
-geo_model.structural_frame.get_element_by_name("Stonepark_WAL").color = "#72A473"
-geo_model.structural_frame.get_element_by_name("Stonepark_ABL").color = "#1D3943"
-geo_model.structural_frame.basement_color = "#8B4220"
-
-geo_model.update_transform()
-
-
 gp.compute_model(
     geo_model,
     engine_config=gp.data.GemPyEngineConfig(
@@ -219,6 +85,3 @@ gempy_vista = gpv.plot_3d(
     show=True,
     kwargs_plot_structured_grid={'opacity': 0.8}
 )
-
-if ADD_ORIGINAL_MESH := False:
-    gempy_vista.p.add_mesh(triangulated_mesh, color="red", opacity=0.5)
---
+++ vector_geology/stonepark_builder.py
@@ -0,0 +1,127 @@
+﻿import numpy as np
+
+import gempy as gp
+import xarray as xr
+from vector_geology.model_building_functions import optimize_nuggets_for_group
+
+
+def initialize_geo_model(structural_elements: list[gp.data.StructuralElement], extent: list[float],
+                         topography: xr.Dataset, load_nuggets: bool = False
+                         ) -> gp.data.GeoModel:
+
+    structural_group_red = gp.data.StructuralGroup(
+        name="Red",
+        elements=[structural_elements[i] for i in [0, 4, 8]],
+        structural_relation=gp.data.StackRelationType.ERODE
+    )
+
+    # Any, Probably we can decimize this an extra notch
+    structural_group_green = gp.data.StructuralGroup(
+        name="Green",
+        elements=[structural_elements[i] for i in [5]],
+        structural_relation=gp.data.StackRelationType.ERODE
+    )
+
+    # Blue range 2 cov 4
+    structural_group_blue = gp.data.StructuralGroup(
+        name="Blue",
+        elements=[structural_elements[i] for i in [2, 3]],
+        structural_relation=gp.data.StackRelationType.ERODE
+    )
+
+    structural_group_intrusion = gp.data.StructuralGroup(
+        name="Intrusion",
+        elements=[structural_elements[i] for i in [1]],
+        structural_relation=gp.data.StackRelationType.ERODE
+    )
+    
+
+    
+    structural_groups = [structural_group_intrusion, structural_group_green, structural_group_blue, structural_group_red]
+    structural_frame = gp.data.StructuralFrame(
+        structural_groups=structural_groups[2:],
+        color_gen=gp.data.ColorsGenerator()
+    )
+    # TODO: If elements do not have color maybe loop them on structural frame constructor?
+
+    geo_model: gp.data.GeoModel = gp.create_geomodel(
+        project_name='Tutorial_ch1_1_Basics',
+        extent=extent,
+        resolution=[20, 10, 20],
+        refinement=6,  # * Here we define the number of octree levels. If octree levels are defined, the resolution is ignored.
+        structural_frame=structural_frame
+    )
+    
+    if topography is not None:
+        gp.set_topography_from_arrays(
+            grid=geo_model.grid,
+            xyz_vertices=topography.vertex.values
+        )
+    
+    if load_nuggets:
+        apply_optimized_nuggets(
+            geo_model=geo_model,
+            loaded_nuggets_red=(np.load("nuggets_Red.npy")),
+            loaded_nuggets_blue=(np.load("nuggets_Blue.npy")),
+            loaded_nuggets_green=(np.load("nuggets_Green.npy"))
+        )
+
+    geo_model.structural_frame.get_element_by_name("Stonepark_KKR").color = "#A46283"
+    geo_model.structural_frame.get_element_by_name("Stonepark_LGR").color = "#6394A4"
+    geo_model.structural_frame.get_element_by_name("Stonepark_WAL").color = "#72A473"
+    geo_model.structural_frame.get_element_by_name("Stonepark_ABL").color = "#1D3943"
+    geo_model.structural_frame.basement_color = "#8B4220"
+
+    geo_model.update_transform()
+
+    return geo_model
+
+
+def optimize_nuggets_for_whole_project(geo_model: gp.data.GeoModel):
+    geo_model.interpolation_options.kernel_options.range = 0.7
+    geo_model.interpolation_options.kernel_options.c_o = 4
+    optimize_nuggets_for_group(
+        geo_model=geo_model,
+        structural_group=geo_model.structural_frame.get_group_by_name('Red'),
+        plot_evaluation=False,
+        plot_result=True
+    )
+    geo_model.interpolation_options.kernel_options.range = 2
+    geo_model.interpolation_options.kernel_options.c_o = 4
+    optimize_nuggets_for_group(
+        geo_model=geo_model,
+        structural_group=geo_model.structural_frame.get_group_by_name('Blue'),
+        plot_evaluation=False,
+        plot_result=False
+    )
+    if False:
+        optimize_nuggets_for_group(
+            geo_model=geo_model,
+            structural_group=geo_model.structural_frame.get_group_by_name('Green'),
+            plot_evaluation=False,
+            plot_result=True
+        )
+
+
+def apply_optimized_nuggets(geo_model: gp.data.GeoModel, loaded_nuggets_red, loaded_nuggets_blue, loaded_nuggets_green):
+    gp.modify_surface_points(
+        geo_model,
+        slice=None,
+        elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Red').elements],
+        nugget=loaded_nuggets_red
+    )
+    if True:  # Ignore OB
+        gp.modify_surface_points(
+            geo_model,
+            slice=None,
+            elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Blue').elements],
+            nugget=loaded_nuggets_blue
+        )
+    if False:
+        gp.modify_surface_points(
+            geo_model,
+            slice=None,
+            elements_names=[element.name for element in geo_model.structural_frame.get_group_by_name('Green').elements],
+            nugget=loaded_nuggets_green
+        )
+
