d5bf1f54f70b04b485ae93e4a8b06bd3ee8f62f6
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-12-06 09:45:25 +0100

    to extend box dimensions and modified interpolation method
    Added a new function 'extend_box' in vector_geology/utils.py to extend the dimensions of a given extent by a certain percentage. This will allow more flexibility when handling geological vectors. Meanwhile, changed the interpolation method from 'linear' to 'nearest' in topography.py to improve data resolution and provide more accurate numerical results.

--- vector_geology/utils.py
+++ vector_geology/utils.py
@@ -8,3 +8,23 @@ def load_omf(env_name='PATH_TO_STONEPARK'):
     omf = omfvista.load_project(path)
     return omf
 
+
+
+def extend_box(extent, percentage=5):
+    # Unpack the extent
+    xmin, xmax, ymin, ymax, zmin, zmax = extent
+
+    # Calculate the increments for each dimension
+    x_increment = (xmax - xmin) * (percentage / 100.0) / 2
+    y_increment = (ymax - ymin) * (percentage / 100.0) / 2
+    z_increment = (zmax - zmin) * (percentage / 100.0) / 2
+
+    # Extend the extents
+    new_xmin = xmin - x_increment
+    new_xmax = xmax + x_increment
+    new_ymin = ymin - y_increment
+    new_ymax = ymax + y_increment
+    new_zmin = zmin - z_increment
+    new_zmax = zmax + z_increment
+
+    return [new_xmin, new_xmax, new_ymin, new_ymax, new_zmin, new_zmax]
