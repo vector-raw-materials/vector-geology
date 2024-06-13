8503f0420bf1e6bb42d23d73b6e08e3065d934f2
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-06-13 15:10:55 +0200

    [DOC] Vector as external examples

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ import numpy as np
 # External examples:
 sys.path.insert(0, os.path.abspath('.'))
 import make_external_gallery
-make_external_gallery.make_example_gallery()
 
+make_external_gallery.make_example_gallery()
 
 # -- PyVista settings -----------------------------------------------------
 
@@ -33,25 +33,26 @@ pyvista.FIGURE_PATH = os.path.join(os.path.abspath('./images/'), 'auto-generated
 if not os.path.exists(pyvista.FIGURE_PATH):
     os.makedirs(pyvista.FIGURE_PATH)
 
-import gempy # ! This seems important to load pykeops c++ library properly
+import gempy  # ! This seems important to load pykeops c++ library properly
+
 sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # Load extensions
 extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon',
-    'sphinx_gallery.gen_gallery',
-    "pyvista.ext.plot_directive",
-    "sphinx_design",
-    'sphinx_automodapi.automodapi',
-    'sphinx_automodapi.smart_resolver',
+        'sphinx.ext.intersphinx',
+        'sphinx.ext.todo',
+        'sphinx.ext.coverage',
+        'sphinx.ext.mathjax',
+        'sphinx.ext.viewcode',
+        'sphinx.ext.githubpages',
+        'sphinx.ext.napoleon',
+        'sphinx_gallery.gen_gallery',
+        "pyvista.ext.plot_directive",
+        "sphinx_design",
+        'sphinx_automodapi.automodapi',
+        'sphinx_automodapi.smart_resolver',
 ]
 
 linkcheck_retries = 3
@@ -59,14 +60,14 @@ linkcheck_timeout = 500
 
 autosummary_generate = True
 add_module_names = True
-numpydoc_show_class_members=False
+numpydoc_show_class_members = False
 
 intersphinx_mapping = {
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'matplotlib': ('https://matplotlib.org/', None),
-    'pyvista': ('https://docs.pyvista.org/', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-    'xarray': ('http://xarray.pydata.org/en/stable/', None)
+        'numpy'     : ('https://numpy.org/doc/stable/', None),
+        'matplotlib': ('https://matplotlib.org/', None),
+        'pyvista'   : ('https://docs.pyvista.org/', None),
+        'pandas'    : ('https://pandas.pydata.org/pandas-docs/stable/', None),
+        'xarray'    : ('http://xarray.pydata.org/en/stable/', None)
 }
 
 napoleon_google_docstring = True
@@ -103,68 +104,70 @@ todo_include_todos = True
 
 # -- Sphinx Gallery Options
 sphinx_gallery_conf = {
-    # path to your examples scripts
-    "examples_dirs": [
-        "../../examples/",
-    ],
-    # path where to save gallery generated examples
-    "gallery_dirs": [
-        "examples",
-    ],
-    # Patter to search for example files
-    "filename_pattern": r"\.py",
-    # Remove the "Download all examples" button from the top level gallery
-    "download_all_examples": False,
-    # Sort gallery example by file name instead of number of lines (default)
-    "within_subsection_order": FileNameSortKey,
-    # directory where function granular galleries are stored
-    "backreferences_dir": 'gen_modules/backreferences',
-    # Modules for which function level galleries are created.  In
-    "doc_module": ('subsurface', 'numpy', 'pandas'),
-    "image_scrapers": ('pyvista', 'matplotlib'),
-    'first_notebook_cell': ("%matplotlib inline\n"
-                            "from pyvista import set_plot_theme\n"
-                            "set_plot_theme('document')"),
-    'reference_url': {
-        # The module you locally document uses None
-        'subsurface': None,
-        'numpy': 'https://numpy.org/doc/stable/'
+        # path to your examples scripts
+        "examples_dirs"          : [
+                "../../examples/",
+        ],
+        # path where to save gallery generated examples
+        "gallery_dirs"           : [
+                "examples",
+        ],
+        # Patter to search for example files
+        "filename_pattern"       : r"\.py",
+        # Remove the "Download all examples" button from the top level gallery
+        "download_all_examples"  : False,
+        # Sort gallery example by file name instead of number of lines (default)
+        "within_subsection_order": FileNameSortKey,
+        # directory where function granular galleries are stored
+        "backreferences_dir"     : 'gen_modules/backreferences',
+        # Modules for which function level galleries are created.  In
+        "doc_module"             : ('subsurface', 'numpy', 'pandas'),
+        "image_scrapers"         : ('pyvista', 'matplotlib'),
+        'first_notebook_cell'    : ("%matplotlib inline\n"
+                                    "from pyvista import set_plot_theme\n"
+                                    "set_plot_theme('document')"),
+        'reference_url'          : {
+                # The module you locally document uses None
+                'subsurface': None,
+                'numpy'     : 'https://numpy.org/doc/stable/'
 
-    },
+        },
 }
 
-linkcheck_ignore = [r'https://github.com/cgre-aachen/gempy_data/raw/master/',
-                    r'https://raw.githubusercontent.com/softwareunderground/subsurface/main/tests/data/borehole/'
-                    ]
+linkcheck_ignore = [
+        r'https://github.com/cgre-aachen/gempy_data/raw/master/',
+        r'https://raw.githubusercontent.com/softwareunderground/subsurface/main/tests/data/borehole/'
+]
 
 linkcheck_request_header = {
-    '*': {'Accept': 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8'},
-    'https://github.com': {}
+        '*'                 : {
+                'Accept': 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8'
+        },
+        'https://github.com': {}
 }
-
 
 # -- Options for HTML output ----------------------------------------------
 html_static_path = ['_static']
 html_logo = '_static/logos/vector-logo.png'
 html_favicon = '_static/logos/favicon.ico'
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'searchbox.html',
-    ]
+        '**': [
+                'about.html',
+                'navigation.html',
+                'searchbox.html',
+        ]
 }
 
 html_context = {
-    'menu_links_name': 'Links',
-    'menu_links': [
-        ('<i class="fa fa-link fa-fw"></i> SWUNG',
-         'https://softwareunderground.org'),
-        ('<i class="fa fa-slack fa-fw"></i> Slack',
-         'https://swu.ng/slack'),
-        ('<i class="fa fa-github fa-fw"></i> Source Code',
-         'https://github.com/softwareunderground/subsurface'),
-    ],
+        'menu_links_name': 'Links',
+        'menu_links'     : [
+                ('<i class="fa fa-link fa-fw"></i> SWUNG',
+                 'https://softwareunderground.org'),
+                ('<i class="fa fa-slack fa-fw"></i> Slack',
+                 'https://swu.ng/slack'),
+                ('<i class="fa fa-github fa-fw"></i> Source Code',
+                 'https://github.com/softwareunderground/subsurface'),
+        ],
 }
 
 htmlhelp_basename = 'visual-bayesic-doc'
@@ -173,6 +176,7 @@ htmlhelp_basename = 'visual-bayesic-doc'
 warnings.filterwarnings("ignore", category=UserWarning,
                         message='Matplotlib is currently using agg, which is a'
                                 ' non-GUI backend, so cannot show the figure.')
+
 
 # -- CSS fixes --
 def setup(app):
