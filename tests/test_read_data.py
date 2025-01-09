6fe445fd0fc89efb678ca89117e4243597246e2c
MigueldelaVarga <miguel@terranigma-solutions.com>
2025-01-09 15:15:51 +0100

    Refactor test to use read_unstructured_topography.
    Replaced manual DXF parsing with the `read_unstructured_topography` function for better readability and maintainability. Simplified the test logic by leveraging higher-level abstractions and improved data handling with `DXFEntityType.POLYLINE`.

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -4,37 +4,18 @@ import dotenv
 
 from subsurface import TriSurf, UnstructuredData, PointSet
 from subsurface.modules.reader import read_unstructured_topography, read_structured_topography
+from subsurface.modules.reader.mesh.dxf_reader import DXFEntityType
 from subsurface.modules.visualization import to_pyvista_mesh, pv_plot, to_pyvista_points
 
 dotenv.load_dotenv()
 
-def test_read_topography():
+
+def test_read_topography_I():
     file_path = os.getenv("PATH_TO_TOPOGRAPHY")
-    import ezdxf
-    import numpy as np
-    doc = ezdxf.readfile(file_path)
-    vertices = []
-
-    # Access the ENTITIES section
-    for entity in doc.entities:
-        # Check if the entity is a POLYLINE
-        if entity.dxftype() == "POLYLINE":
-            # Iterate over all vertices in the POLYLINE
-            for vertex in entity.vertices:
-                # Extract X, Y, Z coordinates
-                x, y, z = vertex.dxf.location.xyz
-                vertices.append([x, y, z])
-
-    # Convert to a NumPy array
-    foo = np.array(vertices)
-    unstruct = UnstructuredData.from_array(
-        vertex=foo,
-        cells="points"
+    unstruct = read_unstructured_topography(
+        path=file_path,
+        additional_reader_kwargs={'entity_type': DXFEntityType.POLYLINE}
     )
-    
-    # unstruct = read_unstructured_topography(filepath)
-    # # struct = read_structured_topography(topo_path)
-    # 
-    # ts = TriSurf(mesh=unstruct)
-    s = to_pyvista_points(PointSet(data=unstruct))
+    ts = TriSurf(mesh=unstruct)
+    s = to_pyvista_mesh(ts)
     pv_plot([s], image_2d=False)
